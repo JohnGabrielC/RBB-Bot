1dbfe9b81adb0f3b49cf958ca83e9ce4d6c85e95
John Gabriel Camaclang <64269071+JohnGabrielC@users.noreply.github.com>
2021-09-02 19:37:09 -0400

    Dictionaries used to make it easier looking

--- rbb.py
+++ rbb.py
@@ -2,12 +2,21 @@ import discord
 import os
 import json
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
-roys = ("rbb", "Rbb", "RBb", "RBB")
+roys = ["rbb", "Rbb", "RBb", "RBB", "rBb", "RbB", "rbB", "rBB"]
+commands = {"roys big": ["balls"],
+            "roy's big": ["balls"],
+            "roys": ["got big balls", "big balls",
+                    "fat juicy delicious scrumptious ginormous jumbo cawck balls"],
+            "roy's": ["got big balls", "big balls",
+                    "fat juicy delicious scrumptious ginormous jumbo cawck balls"],
+            "ken": ["is nice", "has hella girls"],
+            "ray": ["is nice", "has hella girls"]} 
 
 def load_counters():
     with open("counters.json", "r") as f:
@@ -20,11 +29,14 @@ def save_counters(counters):
 
 @client.event
 async def on_message(message):
-    if message.content in roys:
+    if message.content.lower() in commands:
+        ran_num = random.randrange(0, len(commands[message.content]))
+        await message.channel.send("{}".format(commands[message.content][ran_num]))
+
+    elif message.content.lower() in roys:
         counters = load_counters()
         counters["rbb"] += 1
-        await message.channel.send("rbb counter: {}".format(str(counters["rbb"])))
+        await message.channel.send("Rbb Counter: {}".format(str(counters["rbb"])))
         save_counters(counters)
-
 
 client.run(TOKEN)
